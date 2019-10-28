635f6d22fb9d14f5f1bad24753b0e730a4a0a8cb
Skinkeve <Martinhansen4780@gmail.com>
2019-10-28 12:31:47 +0100

    skabte basics

---
+++ unittest_karakterbog.py
@@ -0,0 +1,21 @@
+import unittest
+import karakterbog
+
+
+class unittest(unittest.TestCase):
+
+    def testgetInput(self):
+        self.assertIsInstance(user, str)
+
+
+    def testsetUserVars(self):
+        self.assertIsInstance(alias, str)
+        self.assertIsInstance(enemy, str)
+        self.assertIsInstance(greeting, str)
+
+
+    #def greetUser():
+
+
+a = unittest()
+a.testsetUserVars()
